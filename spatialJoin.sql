6fab9ee3fe581a3dbe68b927e9c23f59ff23cbb9
Ittipat Prapaprasert <iprapaprasert@gmail.com>
2024-02-14 23:08:56 +0700

    add sql query

---
+++ spatialJoin.sql
@@ -0,0 +1,32 @@
+-- Add column MerchantPoint
+ALTER TABLE Klk4.ActiveMerchant
+ADD MerchantPoint Geography;
+GO
+
+-- Add spatial index
+CREATE SPATIAL INDEX SIndx_ActiveMerchant4_MerchantPoint
+ON Klk4.ActiveMerchant(MerchantPoint);
+GO
+
+-- Update MerchantPoint in MerchantPoint Column
+UPDATE Klk4.ActiveMerchant
+SET MerchantPoint = 
+    CASE 
+        WHEN MerchantLatitude IS NULL OR MerchantLongitude IS NULL 
+		THEN NULL 
+	    ELSE geography::Point(MerchantLatitude, MerchantLongitude, 4326) 
+	END;
+GO
+
+-- Add column ProvinceID
+ALTER TABLE Klk4.ActiveMerchant
+ADD ProvinceID int;
+GO
+
+-- Find ProvinceID
+UPDATE Klk4.ActiveMerchant
+SET ProvinceID =
+    (SELECT ProvinceID
+    FROM Spatial.Gistda.ProvinceShape poly
+    WHERE poly.Geog.STIntersects(MerchantPoint) = 1);
+GO
